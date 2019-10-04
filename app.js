d186dac8bd016dbbc22c80695e3515a153fbd114
Michael Kallies <michaelkallies88@gmail.com>
2019-10-04 14:03:53 -0400

    custom header

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Welcome to michaelkallies.ca')
+app.get("/", (req, res) => {
+  res.send("Welcome to michaelkallies.ca")
+})
+
+app.get("/test", (req, res) => {
+  res.set("X-Custom-Header", "wooooo")
+  res.status(418).send("blah")
 })
 
 app.listen(port, () => console.log(`app running on port ${port}`))
